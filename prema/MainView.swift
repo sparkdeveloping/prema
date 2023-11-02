df5e5dbe732e5968ab30027483563591e6395b36
Denzel <sbj.developing@gmail.com>
2023-11-02 13:23:25 -0500

    ASDasD

--- prema/MainView.swift
+++ prema/MainView.swift
@@ -1,14 +1,15 @@
 //
-//  SupportView.swift
+//  MainView.swift
 //  prema
 //
-//  Created by Denzel Nyatsanza on 9/17/23.
+//  Created by Denzel Nyatsanza on 11/2/23.
 //
 
 import SwiftUI
 
-struct SupportView: View {
+struct MainView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
+
